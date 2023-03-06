010b1f5271e3f8a972cfc447313a98b5f1699e8b
MarkStarikov <starikov.marck@gmail.com>
2023-03-06 13:18:14 +0200

    EmptyProject+Main

---
+++ Employee/src/Employee/Main.java
@@ -0,0 +1,10 @@
+package Employee;
+
+public class Main {
+    public static void main(String[] args) {
+        Employer employer = new Employer("Ivanov Ivan Ivanovich", "Manager", "ivan@gmail.com", +9379992, 37 );
+        Employer employer1 = new Employer( "Petrenko Petro Petrovuch", "Owner", "petrenco.ukr.net", +7090393, 40);
+    }
+
+
+}
