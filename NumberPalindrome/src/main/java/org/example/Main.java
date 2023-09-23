51c4f125b5562f0ededa7ddc59d7d4afc4421c6b
MarkStarikov <starikov.marck@gmail.com>
2023-09-23 13:34:10 +0300

    leetcode

---
+++ NumberPalindrome/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Checker solution = new Checker();
+        System.out.println(solution.isPallindrome(12321));
+    }
+}
