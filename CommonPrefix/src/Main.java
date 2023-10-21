4ed3824e8e4d7876455898a11d8eb306369522fe
MarkStarikov <starikov.marck@gmail.com>
2023-10-21 20:46:18 +0300

    leetcode

---
+++ CommonPrefix/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        String [] words = {"condition", "conquest", "congratulation", "construction", "continuous"};
+        System.out.println(longestCommonPrefix(words));
+    }
+    public static String longestCommonPrefix(String [] strs){
+        if(strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i <strs.length ; i++) {
+            while (strs[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0, prefix.length()-1);
+            }
+        }
+        return prefix;
+    }
+}
