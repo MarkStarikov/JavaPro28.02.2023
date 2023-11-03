9d442d24ba202695908e7221c556bda17cc1a379
MarkStarikov <starikov.marck@gmail.com>
2023-11-03 12:52:36 +0200

    leetcode

---
+++ RotateArray/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Worker exam = new Worker();
+        int k = 3;
+        int [] numbers = { 3, 5, 6, 9, 30, 34};
+        exam.rotate(numbers, k);
+    }
+}
